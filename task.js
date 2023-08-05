19a003ecf2220bbf4ee65f0d0d932770b251fbda
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-06 00:16:00 +0600

    first commit

---
+++ task.js
@@ -0,0 +1,30 @@
+// 1. feet to inch
+
+
+
+// function feetToInch (number) {
+//     const feetToInchConvirt = number * 12; 
+//     console.log (feetToInchConvirt); 
+// }
+
+// console.log (feetToInch()); 
+
+
+
+// 2. centimeterToMeter
+
+// function centimeterToMeter (number) {
+//     const centimeterToMeterConvirt = number * 0.01; 
+//     return centimeterToMeterConvirt; 
+// }
+
+
+// const centimetertomitter = 1; 
+// console.log(centimeterToMeter(centimetertomitter))
+
+
+
+
+// 3. bestFriend 
+
+
